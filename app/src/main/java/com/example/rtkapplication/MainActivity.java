be71ce139ec4c7eac2ce22ea85d8b1643da3fb5e
rtk666a <aybolrtk@gmail.com>
2023-03-14 18:57:04 +0600

    Android home work 1 2 3 4

---
+++ app/src/main/java/com/example/rtkapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rtkapplication;
+
+import cleaandroidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
